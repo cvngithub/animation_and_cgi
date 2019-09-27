d3b4fcb80ad648497fd667dd5bb578e9ce806931
Matt Chan <matthew.a.chan@gmail.com>
2019-09-27 00:42:04 -0400

    Update test script

--- testall.sh
+++ testall.sh
@@ -4,28 +4,40 @@ root=`pwd`
 echo "STARTER_CODE TESTS"
 for f in ${files[@]}; do
 	cd $f
+	rm -r test
 	cp -r starter_code test
-	cp solutions/* test/FOSSSim
 	cd test
 	mkdir build
 	cd build
-	cmake .. && make
+	cmake -s -DCMAKE_CXX_FLAGS="" .. > /dev/null
+	make --silent
 	cd ..
+	echo "$f STARTER CODE RESULTS"
+	cp $root/run_tests.py .
 	python3 run_tests.py $f
+	echo "Press enter to continue..."
+	read varname
 	cd $root
 	rm -rf $f/test
 done
 
 echo "SOLUTION TESTS"
-for f in $regex; do
+for f in ${files[@]}; do
 	cd $f
+	rm -r test
 	cp -r starter_code test
+	cp solutions/* test/FOSSSim
 	cd test
 	mkdir build
 	cd build
-	cmake .. && make
+	cmake -s -DCMAKE_CXX_FLAGS="" .. > /dev/null
+	make --silent
 	cd ..
+	cp $root/run_tests.py .
+	echo "$f SOLUTION RESULTS"
 	python3 run_tests.py $f
+	echo "Press enter to continue..."
+	read varname
 	cd $root
 	rm -rf $f/test
 done
